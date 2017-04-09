7bf8ed4d7487990d238326960cf69a2f212ac531
pessonljxpd <903513623lj@gmail.com>
2017-04-09 23:09:21 +0800

    创建DashboardComponent；添加<nav/>；

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import {HeroDetailComponent} from './hero-detail.component';
 import {HeroesComponent} from './heroes.component';
 import {HeroService} from './hero.service';
 import {RouterModule} from '@angular/router';
+import {DashboardComponent} from "./dashboard.component";
 
 @NgModule({
   imports: [
@@ -14,12 +15,21 @@ import {RouterModule} from '@angular/router';
     FormsModule,
     RouterModule.forRoot([
       {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
         path: 'heroes',
         component: HeroesComponent
       }
     ])
   ],
-  declarations: [AppComponent, HeroesComponent, HeroDetailComponent],
+  declarations: [AppComponent, DashboardComponent, HeroesComponent, HeroDetailComponent],
   bootstrap: [AppComponent],
   providers: [HeroService]
 })
---
+++ src/app/dashboard.component.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by Shelly on 2017-4-9.
+ */
+
+import {Component} from "@angular/core";
+@Component({
+  selector: 'my-dashboard',
+  template: `
+    <h3>My Dashboard</h3>
+  `
+})
+
+export class DashboardComponent {
+}
