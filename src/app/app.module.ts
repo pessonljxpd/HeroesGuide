946caa0ead8c262d5f57454be6efdec10f30fbcc
pessonljxpd <903513623lj@gmail.com>
2017-04-10 19:04:16 +0800

    跟着官网学Angular之Http：完成HeroSearchComponent功能；修复因为rx库导包时路径不完整所导致的找不到'rxjs'的问题；

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import {NgModule}      from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
-import {RouterModule} from '@angular/router';
 
 import {AppComponent}  from './app.component';
 import {HeroDetailComponent} from './hero-detail.component';
@@ -12,6 +11,7 @@ import {AppRoutingModule} from './app-routing.module';
 import {HttpModule} from '@angular/http';
 import {InMemoryDataService} from './in-memory-data.service';
 import {InMemoryWebApiModule} from 'angular-in-memory-web-api';
+import {HeroSearchComponent} from './hero-search.component';
 
 @NgModule({
   imports: [
@@ -21,7 +21,7 @@ import {InMemoryWebApiModule} from 'angular-in-memory-web-api';
     InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
-  declarations: [AppComponent, DashboardComponent, HeroesComponent, HeroDetailComponent],
+  declarations: [AppComponent, DashboardComponent, HeroSearchComponent, HeroesComponent, HeroDetailComponent],
   providers: [HeroService],
   bootstrap: [AppComponent]
 })
