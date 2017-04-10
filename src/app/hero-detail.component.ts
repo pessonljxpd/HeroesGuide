e16100c58788374e12092c6f543688ad573bbd74
pessonljxpd <903513623lj@gmail.com>
2017-04-10 16:33:39 +0800

    跟着官网学Angular之Http：在hero.service中添加update方法，在hero-detail页面更新hero数据；

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -23,7 +23,12 @@ export class HeroDetailComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.route.params.switchMap((params: Params) => this.heroService.getHero(+params['id'])).subscribe(hero => this.hero = hero);
+    this.route.params.switchMap((params: Params) => this.heroService.getHero(+params['id']))
+      .subscribe(hero => this.hero = hero);
+  }
+
+  save(): void {
+    this.heroService.update(this.hero).then(() => this.goBack());
   }
 
   goBack(): void {
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,34 +2,41 @@
  * Created by Shelly on 2017-4-9.
  */
 import {Injectable} from '@angular/core';
-import {Http} from '@angular/http';
+import {Http, Headers} from '@angular/http';
 import 'rxjs/add/operator/toPromise';
 import {Hero} from './hero';
 
 @Injectable()
 export class HeroService {
 
+  private headers = new Headers({'content-type': 'application/json'});
   private heroesUrl = 'api/heroes';
 
   constructor(private http: Http) {
   }
 
   getHeroes(): Promise<Hero[]> {
-    return this.http.get(this.heroesUrl).toPromise().then(response => response.json().data as Hero[]).catch(this.handleErro);
+    return this.http.get(this.heroesUrl).toPromise()
+      .then(response => response.json().data as Hero[])
+      .catch(this.handleErro);
+  }
+
+  getHero(id: number): Promise<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get(url).toPromise()
+      .then(response => response.json().data as Hero)
+      .catch(this.handleErro);
+  }
+
+  update(hero: Hero): Promise<Hero> {
+    const url = `${this.heroesUrl}/${hero.id}`;
+    return this.http.put(url, JSON.stringify(hero), {headers: this.headers})
+      .toPromise().then(() => hero)
+      .catch(this.handleErro);
   }
 
   private handleErro(error: any): Promise<any> {
     console.error('An error occurred', error);
     return Promise.reject(error.message || error);
-  }
-
-  getHeroesLowly(): Promise<Hero[]> {
-    return new Promise(resolve => {
-      setTimeout(() => resolve(this.getHeroes()), 2000);
-    });
-  }
-
-  getHero(id: number): Promise<Hero> {
-    return this.getHeroes().then(heroes => heroes.find(hero => hero.id === id));
   }
 }
